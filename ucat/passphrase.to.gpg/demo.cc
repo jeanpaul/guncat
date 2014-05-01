1d737738747720b4d2ff08bb9975fd0426f9f55e
Frank B. Brokken <f.b.brokken@rug.nl>
2014-05-01 11:02:24 +0200

    Changed the passphrase.to.gpg POC: allowing passphrase retries

--- ucat/passphrase.to.gpg/demo.cc
+++ ucat/passphrase.to.gpg/demo.cc
@@ -1,3 +1,4 @@
+// compile with g++ --std=c++0x -pthread demo.cc -lbobcat 
 
 #include <string>
 #include <iostream>
@@ -10,60 +11,88 @@
 #include <bobcat/process>
 #include <bobcat/ofdstream>
 #include <bobcat/ifdstream>
+#include <bobcat/exception>
 
 using namespace std;
 using namespace FBB;
 
-void inStream(Process *gpg, istream *in)
+string passphrase;
+
+void getPassphrase()
 {
-    *gpg << in->rdbuf();
-    *gpg << eoi;
-}
-    
-void runPGP(string const &out, string line)
-{
-    
-    Pipe pipe;
-    {
-        OFdStream writePwd(pipe.writeFd());
-        writePwd << line << endl;
-    }
 
-    line = "/usr/bin/gpg --passphrase-fd " + to_string(pipe.readFd()) +
-                " --batch --quiet --output " + out + " --decrypt";
-
-    Process gpg(Process::CIN, line);
-
-    ifstream in("hein");
-
-    gpg.start();
-
-    thread inThread(inStream, &gpg, &in);
-    inThread.join();
-}
-
-int main(int argc, char **argv)
-{
-    cout << "passphrase: " << flush;
+    cerr << "passphrase: " << flush;
 
     struct termios tty;
     tcgetattr(STDIN_FILENO, &tty);
     tty.c_lflag &= ~ECHO;
     tcsetattr(STDIN_FILENO, TCSANOW, &tty);
 
-    string line;
-    getline(cin, line);
+    getline(cin, passphrase);
 
     tty.c_lflag |= ECHO;
     tcsetattr(STDIN_FILENO, TCSANOW, &tty);
-
-
-    runPGP("out1", line);
-    runPGP("out2", line);
-
-//    gpg.waitForChild();
 }
 
+void inStream(Process *gpg, istream *in, int *exitValue)
+{
+    *gpg << in->rdbuf();
+    gpg->close();
+    *exitValue = gpg->waitForChild();
+}
+    
+void runPGP(string const &out)
+{
+    for (size_t attempt = 0; attempt != 3; ++attempt)
+    {
+        Pipe pipe;
+        {
+            OFdStream writePwd(pipe.writeFd());
+            writePwd << passphrase << endl;
+        }
+    
+        string line = 
+                "/usr/bin/gpg --passphrase-fd " + to_string(pipe.readFd()) +
+                " --batch --quiet --output " + out + " --decrypt";
+    
+        Process gpg(Process::CIN, line);
+    
+        ifstream in("mail");
+    
+        gpg.start();
+    
+        int exitValue;
+        thread inThread(inStream, &gpg, &in, &exitValue);
+        inThread.join();
+    
+        switch (exitValue)
+        {
+            default:
+            return;
+
+            case 2:         // incorrect passphrase
+                cerr << "Incorrect passphrase\n";
+                getPassphrase();
+            break;
+        }
+    }
+    throw Exception() << 
+            "Quitting after three attempts to enter the correct passphrase\n";
+}
+
+int main(int argc, char **argv)
+try
+{
+    getPassphrase();
+
+
+    runPGP("out1");
+    runPGP("out2");
+}
+catch (exception const &exc)
+{
+    cerr << exc.what() << '\n';
+}
 
 
 
