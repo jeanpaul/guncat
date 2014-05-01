749a7a3c46484e7885bde308df98ab59caea8ecb
Frank B. Brokken <f.b.brokken@rug.nl>
2014-05-01 22:16:59 +0200

    WIP: implementing passphrase retries

--- ucat/decryptor/handlegpg.cc
+++ ucat/decryptor/handlegpg.cc
@@ -1,35 +1,60 @@
 #include "decryptor.ih"
 
-void Decryptor::handleGPG(istream &in, string line)
+void Decryptor::handleGPG(istream &in, string const &pgpHeader)
 {
-    Pipe pipe;
-    string passphrase_fd;
+    off_t offset = in.tellg();
 
-    if (not d_passphrase.empty())
+    for (size_t attempt = 0; attempt != 3; ++attempt)
     {
-        passphrase_fd = " --passphrase-fd " + to_string(pipe.readFd());
+        Pipe pipe;
+        string passphrase_fd;
+    
+        if (not d_passphrase.empty())
+        {
+            passphrase_fd = " --passphrase-fd " + to_string(pipe.readFd());
+    
+            OFdStream pwd(pipe.writeFd());
+            pwd << d_passphrase << endl;
+        }
+    
+        Process gpg(Process::CIN | Process::CERR, 
+                        d_gpg + passphrase_fd + d_gpgOptions);
+    
+        gpg.start();
+    
+        gpg << pgpHeader << '\n';
+        string line;
+        do
+        {
+            getline(in, line);
+            gpg << line << '\n';
+        }
+        while (line.find("-----END PGP MESSAGE-----") != 0);
+    
+        gpg.close();
+        int exitValue = gpg.waitForChild();
+    
+        (d_msgName == "-" ? cerr : d_msg)  << gpg.childErrStream().rdbuf();
+    
+        if (exitValue != 2)     // exitValue 2 means: incorrect passphrase
+            return;
+    
+        if (not isatty(STDIN_FILENO))
+            throw Exception() << "Incorrect passphrase, cannot continue\n";
 
-        OFdStream pwd(pipe.writeFd());
-        pwd << d_passphrase << endl;
+        if (not in.seekg(offset, ios::begin))
+            throw Exception() << "File cannot be repositioned "
+                                        "following incorrect passphrase\n";
+
+        getPassphrase();
     }
 
-    Process gpg(Process::CIN | Process::CERR, 
-                    d_gpg + passphrase_fd + d_gpgOptions);
-
-    gpg.start();
-
-    gpg << line << '\n';
-    do
-    {
-        getline(in, line);
-        gpg << line << '\n';
-    }
-    while (line.find("-----END PGP MESSAGE-----") != 0);
-
-    gpg.eoi();
-
-    (d_msgName == "-" ? cerr : d_msg)  << gpg.childErrStream().rdbuf();
+    throw Exception() << 
+            "Quitting after three attempts to enter the correct passphrase\n";
 }
+
+
+
 
 
 
